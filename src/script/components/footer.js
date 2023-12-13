dd1f44f769cc985a7fd3c9dbb200a3d069f19f7f
Omer Faruk Yılmaz <yilmzomrfrk@gmail.com>
2023-12-13 02:00:47 +0100

    footer.js and style.css edited

--- src/script/components/footer.js
+++ src/script/components/footer.js
@@ -1,13 +1,10 @@
 export const footer = `
-
-
+<divclass="additional-text"> 
 <h1>HiCoder 23/24 LMS</h1>
-<div> 
 <p>
   This is a wider card with supporting text below as a natural lead-in to
   additional content. This <br />content is a little bit longer.
 </p>
-</div>
 </div>
 <footer class="footer">
 
